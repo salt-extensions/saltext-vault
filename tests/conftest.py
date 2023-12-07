fe22a27594f16a554871ed94f09c579688635713
jeanluc <lkubb@protonmail.com>
2023-12-07 18:53:27 +0100

    Remove unused fixtures
    Those are leftovers from the previous Salt test suite.
    `base_env_state_tree_root_dir` can be accessed as
    `salt_master.state_tree.base` etc.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,5 @@
 import logging
 import os
-import sys
 
 import pytest
 from pytestshellutils.utils import ports
@@ -39,55 +38,6 @@ def master(salt_factories):
 @pytest.fixture(scope="package")
 def minion(master):
     return master.salt_minion_daemon(random_string("minion-"))
-
-
-@pytest.fixture(scope="session")
-def integration_files_dir(salt_factories):
-    """
-    Fixture which returns the salt integration files directory path.
-    Creates the directory if it does not yet exist.
-    """
-    dirname = salt_factories.root_dir / "integration-files"
-    dirname.mkdir(exist_ok=True)
-    return dirname
-
-
-@pytest.fixture(scope="session")
-def state_tree_root_dir(integration_files_dir):
-    """
-    Fixture which returns the salt state tree root directory path.
-    Creates the directory if it does not yet exist.
-    """
-    dirname = integration_files_dir / "state-tree"
-    dirname.mkdir(exist_ok=True)
-    return dirname
-
-
-@pytest.fixture(scope="session")
-def base_env_state_tree_root_dir(state_tree_root_dir):
-    """
-    Fixture which returns the salt base environment state tree directory path.
-    Creates the directory if it does not yet exist.
-    """
-    dirname = state_tree_root_dir / "base"
-    dirname.mkdir(exist_ok=True)
-    return dirname
-
-
-@pytest.fixture
-def perm_denied_error_log():
-    if sys.platform.startswith("win32"):
-        perm_denied_error_log = (
-            "Unable to create directory "
-            "C:\\ProgramData\\Salt Project\\Salt\\srv\\salt\\minion.  "
-            "Check that the salt user has the correct permissions."
-        )
-    else:
-        perm_denied_error_log = (
-            "Unable to create directory /srv/salt/minion.  "
-            "Check that the salt user has the correct permissions."
-        )
-    return perm_denied_error_log
 
 
 @pytest.fixture(scope="session")
