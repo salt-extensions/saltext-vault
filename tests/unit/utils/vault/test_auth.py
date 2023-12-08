4c7bef869c705e6203f9faf0875b3a66a8f598b3
jeanluc <lkubb@protonmail.com>
2023-12-08 15:46:52 +0100

    More flakyness reduction

--- tests/unit/utils/vault/test_auth.py
+++ tests/unit/utils/vault/test_auth.py
@@ -220,6 +220,7 @@ def test_approle_auth_get_token_store_available(token_store, approle, token):
         assert res == token
 
 
+@pytest.mark.usefixtures("time_stopped")
 def test_approle_auth_get_token_store_empty(token_store_empty, approle, token):
     """
     Ensure a token is returned if no cached token is available
@@ -242,6 +243,7 @@ def test_approle_auth_get_token_invalid(token_store_empty, approle_invalid):
         auth.get_token()
 
 
+@pytest.mark.usefixtures("time_stopped")
 @pytest.mark.parametrize("mount", ["approle", "salt_minions"])
 @pytest.mark.parametrize("approle", ["secret_id", None], indirect=True)
 def test_approle_auth_get_token_login(approle, mount, client, token_store_empty_first, token):
@@ -262,6 +264,7 @@ def test_approle_auth_get_token_login(approle, mount, client, token_store_empty_
     token_store_empty_first.replace_token.assert_called_once_with(res)
 
 
+@pytest.mark.usefixtures("time_stopped")
 @pytest.mark.parametrize("num_uses", [0, 1, 10])
 def test_approle_auth_used_num_uses(
     token_store_empty_first, approle, client, uncached, num_uses, token
@@ -285,6 +288,7 @@ def test_approle_auth_used_num_uses(
         uncached.store.assert_not_called()
 
 
+@pytest.mark.usefixtures("time_stopped")
 def test_approle_auth_used_locally_configured(
     token_store_empty_first, approle, client, uncached, token
 ):
