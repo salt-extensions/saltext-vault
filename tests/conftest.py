b0c74bf6a05710b68aac45ed38616d46a8c85940
jeanluc <lkubb@protonmail.com>
2024-07-23 12:22:58 +0200

    Add SSH fixtures and wrappers skeleton

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,14 @@
 import json
 import logging
 import os
+import shutil
 import subprocess
 import time
+from pathlib import Path
 
 import pytest
 import salt.utils.path
+import salt.utils.platform
 from pytestshellutils.utils import ports
 from pytestshellutils.utils.processes import ProcessResult
 from saltfactories.utils import random_string
@@ -15,6 +18,11 @@ from tests.support.helpers import PatchedEnviron
 from tests.support.vault import vault_enable_auth_method
 from tests.support.vault import vault_enable_secret_engine
 from tests.support.vault import vault_write_policy_file
+
+try:
+    import pwd
+except ImportError:
+    import salt.utils.win_functions
 
 # Reset the root logger to its default level(because salt changed it)
 logging.root.setLevel(logging.WARNING)
@@ -137,6 +145,113 @@ def minion(master, minion_config_defaults, minion_config_overrides):
     return master.salt_minion_daemon(
         random_string("minion-"), defaults=minion_config_defaults, overrides=minion_config_overrides
     )
+
+
+@pytest.fixture(scope="session")
+def current_user():
+    """
+    Get the user associated with the current process.
+    """
+    if salt.utils.platform.is_windows():
+        return salt.utils.win_functions.get_current_user(with_domain=False)
+    return pwd.getpwuid(os.getuid())[0]
+
+
+@pytest.fixture(scope="module")
+def sshd_server(salt_factories, sshd_config_dir):
+    sshd_config_dict = {
+        "Protocol": "2",
+        # Turn strict modes off so that we can operate in /tmp
+        "StrictModes": "no",
+        # Logging
+        "SyslogFacility": "AUTH",
+        "LogLevel": "INFO",
+        # Authentication:
+        "LoginGraceTime": "120",
+        "PermitRootLogin": "without-password",
+        "PubkeyAuthentication": "yes",
+        # Don't read the user's ~/.rhosts and ~/.shosts files
+        "IgnoreRhosts": "yes",
+        "HostbasedAuthentication": "no",
+        # To enable empty passwords, change to yes (NOT RECOMMENDED)
+        "PermitEmptyPasswords": "no",
+        # Change to yes to enable challenge-response passwords (beware issues with
+        # some PAM modules and threads)
+        "ChallengeResponseAuthentication": "no",
+        # Change to no to disable tunnelled clear text passwords
+        "PasswordAuthentication": "no",
+        "X11Forwarding": "no",
+        "X11DisplayOffset": "10",
+        "PrintMotd": "no",
+        "PrintLastLog": "yes",
+        "TCPKeepAlive": "yes",
+        "AcceptEnv": "LANG LC_*",
+        "UsePAM": "yes",
+    }
+    sftp_server_paths = [
+        # Common
+        "/usr/lib/openssh/sftp-server",
+        # CentOS Stream 9
+        "/usr/libexec/openssh/sftp-server",
+        # Arch Linux
+        "/usr/lib/ssh/sftp-server",
+        # Photon OS 5
+        "/usr/libexec/sftp-server",
+    ]
+    sftp_server_path = None
+    for path in sftp_server_paths:
+        if Path(path).exists():
+            sftp_server_path = path
+    if sftp_server_path is None:
+        pytest.fail(f"Failed to find 'sftp-server'. Searched: {sftp_server_paths}")
+    else:
+        sshd_config_dict["Subsystem"] = f"sftp {sftp_server_path}"
+    factory = salt_factories.get_sshd_daemon(
+        sshd_config_dict=sshd_config_dict,
+        config_dir=sshd_config_dir,
+    )
+    with factory.started():
+        yield factory
+
+
+@pytest.fixture(scope="module")
+def known_hosts_file(sshd_server, master, salt_factories):
+    with pytest.helpers.temp_file(
+        "ssh-known-hosts",
+        "\n".join(sshd_server.get_host_keys()),
+        salt_factories.tmp_root_dir,
+    ) as known_hosts_file, pytest.helpers.temp_file(
+        "master.d/ssh-known-hosts.conf",
+        f"known_hosts_file: {known_hosts_file}",
+        master.config_dir,
+    ):
+        yield known_hosts_file
+
+
+@pytest.fixture(scope="module")
+def salt_ssh_roster_file(
+    sshd_server, master, known_hosts_file, current_user
+):  # pylint: disable=unused-argument
+    roster_contents = f"""
+    localhost:
+      host: 127.0.0.1
+      port: {sshd_server.listen_port}
+      user: {current_user}
+    """
+    if salt.utils.platform.is_darwin():
+        roster_contents += "  set_path: $PATH:/usr/local/bin/\n"
+
+    with pytest.helpers.temp_file("roster", roster_contents, master.config_dir) as roster_file:
+        yield roster_file
+
+
+@pytest.fixture(scope="session")
+def sshd_config_dir(salt_factories):
+    config_dir = salt_factories.get_root_dir_for_daemon("sshd")
+    try:
+        yield config_dir
+    finally:
+        shutil.rmtree(str(config_dir), ignore_errors=True)
 
 
 @pytest.fixture(scope="session")
