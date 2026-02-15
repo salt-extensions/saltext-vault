4d7a8d94f9fbf47f3a46ae75ed85b518c9b80531
jeanluc <lkubb@protonmail.com>
2026-02-15 13:09:21 +0100

    Remove IPC_LOCK capability from testing container
    The parameter should be a list of strings. Passing a string there seems
    to cause failure now.
    
    It's needed for Vault's disable_mlock setting, which we don't need
    in testing.

--- tests/conftest.py
+++ tests/conftest.py
@@ -298,7 +298,6 @@ def vault_container_version(
                 "VAULT_DEV_ROOT_TOKEN_ID": "testsecret",
                 "VAULT_LOCAL_CONFIG": json.dumps(config),
             },
-            "cap_add": "IPC_LOCK",
         },
         pull_before_start=True,
         skip_on_pull_failure=True,
